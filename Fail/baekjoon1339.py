a1591911fc23ce0fe73c0229c89e621b84133492
wkd3ogks <wkdeogks17@gmail.com>
2021-12-22 16:21:45 +0900

    baekjoon1339

--- Fail/baekjoon1339.py
+++ Fail/baekjoon1339.py
@@ -1,37 +1,24 @@
 # [Gold 4] 단어 수학
-possible = [i for i in range(10)]
-combine = [[] for _ in range(8)]
-alphaCnt = [0 for i in range(26)]
-alpha = [-1 for i in range(26)]
-number = []
-index = 0
-n = int(input()) 
+# 갯수에 가중치를 줘서(자릿수) 해결하면 되는 문제
+# 살짝 운으로 풀은 느낌? 우연하게 생각함
+# 숫자는 쪼갤 수 있다. 899 = 800 + 90 + 9, 교환법칙이 가능하다.(나중에 더해도 된다) A + B = B + A
+
+n = int(input())
+data = list()
+alpha = [0 for _ in range(26)]
 for _ in range(n):
-    j = 0
-    before = input()
-    i = 8 - len(before)
-    while i < 8:
-        alphaCnt[ord(before[j]) - 65] += 1
-        combine[i].append([before[j], index]) 
-        j += 1
-        i += 1
-    index += 1
-    number.append(0)
-for i in range(8):
-    for j in range(len(combine[i])):
-       combine[i][j].append(alphaCnt[ord(combine[i][j][0]) - 65])
-exponent = 7
-for i in range(8):
-    combine[i].sort(key=lambda x: -x[2])
-    for alpha1, index, cnt in combine[i]:
-        if alpha[ord(alpha1) - 65] == -1:
-            alpha[ord(alpha1) - 65] = possible.pop() 
-        number[index] += alpha[ord(alpha1) - 65] * 10 ** exponent
-    exponent -= 1
+    data.append(input())
+
+for string in data:
+    length = len(string) - 1
+    for char in string:
+        alpha[ord(char) - 65] += 10 ** length
+        length -= 1
+alpha.sort(reverse=True)
+num = 9
 total = 0
-for i in number:
-    total += i
+for i in alpha:
+    total += i * num
+    num -= 1
 print(total)
-
-
 

f2a4c40c69431045ca0b6686d4a6e90120e2c747
wkd3ogks <wkdeogks17@gmail.com>
2022-01-05 23:33:15 +0900

    baekjoon1647

--- Fail/baekjoon1005.py
+++ Fail/baekjoon1005.py
@@ -1,9 +1,13 @@
 # [Gold 3] ACM Craft
-# 시간을 음수로 바꿔서 최소 스패닝 트리
-v, e = map(int, input().split())
-unionSet = set()
-edges = []
-for _ in range(e):
+# 바꿔서 최소 스패닝 트리
+# 사이클이 만들어 질 때 작은 값으로 바꾼다.
+# 햇갈리네..
+n, k = map(int, input().split())
+time = list(map(int, input().split()))
+for _ in range(n):
     edges.append(list(map(int, input().split())))
 edges.sort()
+parent = []
+for _ in range(v):
+
 print(edges)
--- Fail/baekjoon1647.py
+++ Fail/baekjoon1647.py
@@ -1,3 +1,44 @@
 # [Gold 4] 도시 분할 계획
-# 최소 스패닝 트리를 공부해야 함.
 
+# 스패닝 트리에서 가장 큰 값 하나 짤라내면 끝.
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+edges = []
+unionFind = [i for i in range(n)]
+for _ in range(m):
+    n1, n2, value = map(int, input().split())
+    edges.append((n1 - 1, n2 - 1, value))
+edges.sort(key=lambda x:x[2])
+
+def getParent(x):
+    if unionFind[x] == x:
+        return x 
+    return getParent(unionFind[x])
+
+def unionParent(a, b):
+    a = getParent(a)
+    b = getParent(b)
+
+    if a < b:
+        unionFind[b] = a
+    else:
+        unionFind[a] = b
+
+def findParent(a, b):
+    a = getParent(a)
+    b = getParent(b)
+    if a == b:
+        return True
+    else:
+        return False
+big = 0
+total = 0
+for node1, node2, value in edges:
+    if not findParent(node1, node2):
+        if big < value:
+            big = value
+        unionParent(node1, node2)
+        total += value
+print(total - big)

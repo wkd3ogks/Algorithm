f613ead9792a509b126059a00e5277086d337606
wkd3ogks <wkdeogks17@gmail.com>
2022-01-06 14:12:54 +0900

    baekjoon9466

--- Fail/baekjoon9466.py
+++ Fail/baekjoon9466.py
@@ -1,31 +1,32 @@
 # [Gold 3] 텀 프로젝트
-# Union-Find 문제
+# 모든 문제든 단순하게! 생각하자!
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**7)
 
-def dfs(now, cnt, start):
-    if students[now] == -1:
-        return 0
-    if start == students[now] - 1:
-        return cnt 
-    elif now == students[now] - 1:
-        students[now] = -1
-        return 1 
-    else:
-        if students[now] in studentList:
-            studentList.remove(students[now])
-        cnt += 1
-        return dfs(students[now] - 1, cnt, start)
-def solve():
-    total = 0
-    while studentList:
-        student = studentList.pop() - 1
-        total += dfs(student, 1, student)
-    print(n - total)
+input = sys.stdin.readline
+def dfs(studnt, length):
+    global rslt
+    visited[studnt] = True
+    cycle.append(studnt)
+    if visited[students[studnt] - 1] == True:
+        for i in range(length):
+            if cycle[i] == students[studnt] - 1:
+                for j in range(i, length):
+                    rslt.append(cycle[j])
+                return
+        return
+    return dfs(students[studnt] - 1, length + 1)
 
 t = int(input())
 for _ in range(t):
     n = int(input())
+    total = 0
     students = list(map(int, input().split()))
-    studentList = set([i for i in range(n, 0, -1)])
-    solve()
+    visited = [False for _ in range(n)]
+    rslt = []
+    for i in range(n):
+        if visited[i] == False:
+            cycle = []
+            dfs(i, 1)
+    print(n - len(rslt))
+

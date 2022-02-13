609ad092f97524876ed8d6a0ebf0409123cb1b94
wkd3ogks <wkdeogks17@gmail.com>
2022-02-13 12:59:48 +0900

    baekjoon1106

---
+++ DP/baekjoon1106.py
@@ -0,0 +1,19 @@
+# [Silver 1] 호텔
+
+from argon2 import PasswordHasher
+
+
+reqC, cityN = map(int, input().split())
+marketing = []
+for _ in range(cityN):
+    marketing.append(list(map(int, input().split())))
+memo = [1000000 for _ in range(reqC + 1)]
+memo[0] = 0
+for i in range(reqC + 1):
+    for cost, clients in marketing:
+        if i - clients <= 0:
+            if memo[i] > cost:
+                memo[i] = cost
+        elif memo[i] > cost + memo[i - clients]:
+                memo[i] = cost + memo[i - clients]
+print(memo[reqC])

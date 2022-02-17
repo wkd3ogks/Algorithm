741a46a712f6f3c156b8f373f9a43625c3327dfe
wkd3ogks <wkdeogks17@gmail.com>
2022-02-18 00:42:55 +0900

    baekjoon1535

---
+++ DP/baekjoon1535.py
@@ -0,0 +1,25 @@
+# [Silver 2] 안녕
+
+allPerson = int(input()) + 1
+
+# lossHealth[PersonIndex], gainHappy[PersonIndex]
+lossHealth = [0] + list(map(int, input().split()))
+gainHappy = [0] + list(map(int, input().split()))
+
+
+# maxHappy[SumLossHealth] = MaxHappy
+maxHappy = [[0 for _ in range(100)] for __ in range(allPerson)]
+
+for PersonIndex in range(allPerson):
+    for sumLossHealth in range(100):
+        if sumLossHealth >= lossHealth[PersonIndex]:
+            maxHappy[PersonIndex][sumLossHealth] = max(maxHappy[PersonIndex - 1][sumLossHealth - lossHealth[PersonIndex]] + gainHappy[PersonIndex], maxHappy[PersonIndex - 1][sumLossHealth])
+        else:
+            maxHappy[PersonIndex][sumLossHealth] = maxHappy[PersonIndex - 1][sumLossHealth]
+
+
+answer = -1
+for y in range(allPerson):
+    if answer < maxHappy[y][99]:
+        answer = maxHappy[y][99]
+print(answer)
---
+++ Fail/baekjoon2473.py
@@ -0,0 +1,6 @@
+# [Gold 4] 세 용액
+
+alLiquid = int(input())
+liquid = list(map(int, input().split()))
+liquid.sort()
+print(liquid)

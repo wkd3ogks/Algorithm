420bbf3536abaa2938576f54b5789f40ca532718
wkd3ogks <wkdeogks17@gmail.com>
2022-04-02 19:31:12 +0900

    clear Fail Forder

--- Fail/baekjoon1166.py
+++
@@ -1,8 +0,0 @@
-# [Silver 3] 선물
-
-n, l, w, h = map(int, input().split())
-min = l / n
-if min > w / n:
-    min = w / n
-if min > h / n:
-    min = h / n
--- Fail/baekjoon2473.py
+++
@@ -1,6 +0,0 @@
-# [Gold 4] 세 용액
-
-alLiquid = int(input())
-liquid = list(map(int, input().split()))
-liquid.sort()
-print(liquid)
